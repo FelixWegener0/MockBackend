97d84ec2323be6bac92095106fba463d40692143
FWegener <FWegener@adesso-mobile.de>
2023-09-29 09:02:23 +0200

    mock data for frontend server

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask_restful import Api, Resource
 from flask_cors import CORS
 import json
+from time import sleep
 
 app = Flask(__name__)
 cores = CORS(app)
@@ -9,17 +10,29 @@ cores = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 api = Api(app)
 
-file = open("data.json")
-mockData = json.load(file)
+mockDatabase = open("mockdatabase.json")
+mockDatabasejson = json.load(mockDatabase)
 
-class getData(Resource):
+class getThisDaysTempValues(Resource):
     def get(self):
-        return jsonify(mockData)
+        return jsonify(mockDatabasejson["data"])
 
-api.add_resource(getData, "/getData")
+class getCurrentTemp(Resource):
+    def get(self):
+        sleep(5)
+        return jsonify(25)
+    
+class getCurrentHumid(Resource):
+    def get(self):
+        sleep(5)
+        return jsonify(45)
+
+api.add_resource(getThisDaysTempValues, "/getThisDaysTempValues")
+api.add_resource(getCurrentTemp, "/getCurrentTemp")
+api.add_resource(getCurrentHumid, "/getCurrentHumid")
 
 if (__name__ == "__main__"):
     print('run mockBackend Server')
     app.run(host='0.0.0.0')
 
-file.close()
+mockDatabase.close()
